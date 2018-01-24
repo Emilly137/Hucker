3cb8ceb4b02643065a2f11f1f14ea0a5a7ef39c0
putilinadaria137 <putilinadaria137>
2018-01-24 16:25:22 +0200

    Initial commit

---
+++ app/src/main/java/com/hucker/hucker/StartLoadingMenu.java
@@ -0,0 +1,13 @@
+package com.hucker.hucker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StartLoadingMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_loading_menu);
+    }
+}
