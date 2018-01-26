d72a26bbf116527742d772140f54bed3d21a043a
A1doranM <vaselisk1997@gmail.com>
2018-01-26 16:16:45 +0200

    Added animation for start screen and delay before go to next page and Facebook login

--- app/src/main/java/com/hucker/hucker/LoginScreen.java
+++ app/src/main/java/com/hucker/hucker/LoginScreen.java
@@ -1,13 +1,64 @@
 package com.hucker.hucker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 public class LoginScreen extends AppCompatActivity {
+    TextView textView;
+    LoginButton login_button;
+    CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        setContentView(R.layout.activity_login_screen);
+        initializeCotrols();
+
+        loginWithFB();
+    }
+
+    private void initializeCotrols(){
+        callbackManager = CallbackManager.Factory.create();
+        login_button = findViewById(R.id.login_button);
+        textView = findViewById(R.id.textView2);
+    }
+
+
+    private void loginWithFB(){
+        LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                textView.setText("Login Success\n" + loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+                textView.setText("Login Cancelled\n");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                textView.setText("Login error: " + error.getMessage());
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
